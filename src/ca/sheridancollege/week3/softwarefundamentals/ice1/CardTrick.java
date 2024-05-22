67e90202b1f54cb0c423f27e86218f9d112a504e
sin14747 <sin14747@sheridancollege.ca>
2024-05-22 16:45:50 -0400

    committing for the 1St in class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -12,7 +12,10 @@ import java.util.Scanner;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author dancye
+ * @author Kuldeep Singh
+ * @modifier KuldeepSingh
+ * 
+ * 
  */
 public class CardTrick {
     
